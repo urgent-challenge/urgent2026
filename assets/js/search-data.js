9bce643e7ed3a2d1e7333cf7893d543718b394ed
LiChenda <chenda.li@outlook.com>
2025-12-29 05:57:20 +0000

    Deploying to gh-pages from @ urgent-challenge/urgent2026@0801f2bc5dd6709b914645483e5cf6d1e2b81eec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -552,13 +552,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/urgent2026/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/urgent2026/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
